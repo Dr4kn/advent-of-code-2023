c171641f502b96002ce1e7aece9de333dfc41652
Dr4kn <servicesDrkn@gmail.com>
2023-12-10 23:56:15 +0100

    day 8 part 1 done

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,7 +1,24 @@
 fun main() {
 
+    data class Node(val start: String, val left: String, val right: String)
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val directions = input[0]
+        val nodeNetwork = input.drop(2)
+            .map { it.replace(")", "").split(" = (", ", ") }
+            .map { Node(it[0], it[1], it[2]) }
+            .sortedBy { it.start }
+        var steps = 0
+        var currentPosition = 0
+        var nextElement = nodeNetwork[0].start //AAA
+        while (nextElement != "ZZZ") {
+            val index = nodeNetwork.binarySearch { String.CASE_INSENSITIVE_ORDER.compare(it.start, nextElement) }
+            nextElement = if (directions[currentPosition] == 'L') nodeNetwork[index].left else nodeNetwork[index].right
+            currentPosition++
+            if (currentPosition >= directions.length) currentPosition = 0
+            steps++
+        }
+        return steps
     }
 
     fun part2(input: List<String>): Int {
@@ -9,11 +26,11 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day07_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day08_test")
+    check(part1(testInput) == 6)
 //    check(part2(testInput) == 1)
 
-    val input = readInput("Day07")
-//    part1(input).println()
+    val input = readInput("Day08")
+    part1(input).println()
 //    part2(input).println()
 }
