6dd88dcb06a9f9cb50424a572d0002a22ecd5f39
Dr4kn <servicesDrkn@gmail.com>
2023-12-10 20:11:22 +0100

    day 7 part done and setup until day 10

--- src/Day07.kt
+++ src/Day07.kt
@@ -10,16 +10,8 @@ enum class TypeOfHand(number: Int) {
 
 fun main() {
 
-    fun cardValue(cardChar: Char, part1: Boolean): Int {
-        if (part1) return when (cardChar) {
-            'A' -> 14
-            'K' -> 13
-            'Q' -> 12
-            'J' -> 11
-            'T' -> 10
-            else -> cardChar.digitToInt()
-        }
-        return when (cardChar) {
+    fun cardValue(cardChar: Char, withJoker: Boolean): Int {
+        if (withJoker) return when (cardChar) {
             'A' -> 14
             'K' -> 13
             'Q' -> 12
@@ -27,26 +19,34 @@ fun main() {
             'T' -> 10
             else -> cardChar.digitToInt()
         }
+        return when (cardChar) {
+            'A' -> 14
+            'K' -> 13
+            'Q' -> 12
+            'J' -> 11
+            'T' -> 10
+            else -> cardChar.digitToInt()
+        }
     }
 
     data class Hand(val cards: List<Int>, val bid: Int, var typeOfHand: TypeOfHand)
 
-    fun fullHouseOrFourOfAKind(hand: String): TypeOfHand {
+    fun fullHouseOrFourOfAKind(hand: List<Int>): TypeOfHand {
         hand.toSet().forEach { card ->
             if (hand.count { it == card } == 3) return TypeOfHand.FULL_HOUSE
         }
         return TypeOfHand.FOUR_OF_A_KIND
     }
 
-    fun threeOrTwoOfAKind(hand: String): TypeOfHand {
+    fun threeOrTwoOfAKind(hand: List<Int>): TypeOfHand {
         hand.toSet().forEach { card ->
             if (hand.count { it == card } == 3) return TypeOfHand.THREE_OF_A_KIND
         }
         return TypeOfHand.TWO_PAIR
     }
 
-    fun sortHands(parsedInput: List<Hand>): List<Hand> {
-        return parsedInput.sortedWith(compareByDescending<Hand> { it.typeOfHand }
+    fun sortHands(hands: List<Hand>): List<Hand> {
+        return hands.sortedWith(compareByDescending<Hand> { it.typeOfHand }
             .thenBy { it.cards[0] }
             .thenBy { it.cards[1] }
             .thenBy { it.cards[2] }
@@ -57,34 +57,58 @@ fun main() {
 
     fun winningsPerHand(hands: List<Hand>) = hands.mapIndexed { index, hand -> hand.bid * (index + 1) }
 
-    fun part1(input: List<String>): Int {
-        val parsedInput = input.map { hand -> hand.split(""" +""".toRegex()) }
-            .map { hand ->
-                val bid = hand[1].toInt()
-                val cards = hand[0].map { cardValue(it, true) }
-                when (hand[0].toSet().size) {
-                    1 -> Hand(cards, bid, TypeOfHand.FIVE_OF_A_KIND)
-                    2 -> Hand(cards, bid, fullHouseOrFourOfAKind(hand[0]))
-                    3 -> Hand(cards, bid, threeOrTwoOfAKind(hand[0]))
-                    4 -> Hand(cards, bid, TypeOfHand.ONE_PAIR)
-                    5 -> Hand(cards, bid, TypeOfHand.HIGH_CARD)
-                    else -> Hand(cards, hand[1].toInt(), TypeOfHand.FULL_HOUSE)
+    fun bestJokerUsage(hand: List<Int>): List<Int> {
+        val jokers = hand.count { it == 1 }
+        val replacedWithHighest = hand.map { if (it == 1) hand.toSet().sortedDescending()[0] else it }
+        if (jokers == 1 || jokers == 2) {
+            when (hand.toSet().size) {
+                5 -> return replacedWithHighest
+                4, 3, 2 -> {
+                    if (hand.toSet().size == 4 && jokers == 2) return replacedWithHighest
+                    val xOfAKindCards  = hand.sortedDescending()
+                        .groupingBy { it }
+                        .eachCount()
+                        .filter { it.value > 1 }
+                        .toList()[0].first
+                    return hand.map { card -> if (card == 1) xOfAKindCards else card }
                 }
             }
+        }
 
-        return winningsPerHand(sortHands(parsedInput)).sum()
+        if (jokers == 4 || jokers == 3) return replacedWithHighest
+        return hand
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun inputToHand(hand: List<String>, withJoker: Boolean): Hand {
+        val bid = hand[1].toInt()
+        val cardValue = hand[0].map { cardValue(it, withJoker) }
+        val cards = if (withJoker) bestJokerUsage(cardValue) else cardValue
+        return when (cards.toSet().size) {
+            1 -> Hand(cardValue, bid, TypeOfHand.FIVE_OF_A_KIND)
+            2 -> Hand(cardValue, bid, fullHouseOrFourOfAKind(cards))
+            3 -> Hand(cardValue, bid, threeOrTwoOfAKind(cards))
+            4 -> Hand(cardValue, bid, TypeOfHand.ONE_PAIR)
+            5 -> Hand(cardValue, bid, TypeOfHand.HIGH_CARD)
+            else -> Hand(cardValue, bid, TypeOfHand.FULL_HOUSE)
+        }
+    }
+
+    fun parsedInput(input: List<String>, withJoker: Boolean): List<Hand> {
+        return input.map { hand -> hand.split(""" +""".toRegex()) }
+            .map { hand -> inputToHand(hand, withJoker) }
+    }
+
+    fun solver(input: List<String>, withJoker: Boolean): Int {
+        val parsedInput = parsedInput(input, withJoker)
+        return winningsPerHand(sortHands(parsedInput)).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day07_test")
-//    check(part1(testInput) == 6440)
-    check(part2(testInput) == 5905)
+    check(solver(testInput, false) == 6440)
+    check(solver(testInput, true) == 5905)
 
     val input = readInput("Day07")
-//    part1(input).println()
-//    part2(input).println()
+    solver(input, false).println()
+    solver(input, true).println()
 }
