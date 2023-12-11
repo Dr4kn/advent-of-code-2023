8825dea193df52032f7fe5e909bb7f7057e70911
Dr4kn <servicesDrkn@gmail.com>
2023-12-11 20:01:23 +0100

    day 9

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,19 +1,25 @@
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun solver(input: List<String>, part1: Boolean): Long {
+        val reports = input.map { line -> line.split(" ").map { it.toLong() } }
+        var lastDigit: Long = 0
+        for (report in reports) {
+            var reducedReport = if (part1) report else report.reversed()
+            lastDigit += reducedReport.last()
+            while (reducedReport.filter { it == 0.toLong() }.size != reducedReport.size) {
+                reducedReport = reducedReport.windowed(2).map { it[1] - it[0]  }
+                lastDigit += reducedReport.last()
+            }
+        }
+        return lastDigit
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day07_test")
-    check(part1(testInput) == 1)
-//    check(part2(testInput) == 1)
+    val testInput = readInput("Day09_test")
+    check(solver(testInput, true) == 114.toLong())
+    check(solver(testInput, false) == 2.toLong())
 
-    val input = readInput("Day07")
-//    part1(input).println()
-//    part2(input).println()
+    val input = readInput("Day09")
+    solver(input, true).println()
+    solver(input, false).println()
 }
