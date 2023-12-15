e1cfe5b97a83bc60db78bfff7a214353aef237b1
Dr4kn <servicesDrkn@gmail.com>
2023-12-15 22:53:36 +0100

    finished day 3

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,93 +1,92 @@
 fun main() {
-
-    data class Symbol(val column: Int, val row: Int)
-    fun Int.coerceArray(maxLength: Int) =
-        this.coerceAtLeast(0).coerceAtMost(maxLength - 1)
-
-//    fun findNumber(symbols: List<Symbol>, schematic: List<String>) {
-//        val adjacentNumbers: MutableList<Int> = MutableList(0){0}
-//        for (symbol in symbols) {
-//            val rowMaxLength = schematic[0].length
-//            val columnMaxLength = schematic.size
-//            val rowLowerRange = (symbol.row - 3).coerceArray(rowMaxLength)
-//            val rowUpperRange = (symbol.row + 3).coerceArray(rowMaxLength)
-//            schematic[symbol.column-1].substring(rowLowerRange, rowUpperRange).windowed(3)
-//
-//        }
-//    }
-
     fun part1(input: List<String>): Int {
-        val maxLength = input[0].length
-        val schematic = input.flatMap { it.toList() }
-        val symbols = input.flatMap { it.toList() }.mapIndexed{index, symbol ->
-            if(!symbol.isDigit() && symbol != '.') Symbol(index % maxLength, index / maxLength) else null
-        }.filterNotNull()
-
-//        for (symbol in symbols) {
-//            val row = symbol.row
-//            val rowMaxLength = input.size
-//            val column = symbol.column
-//            val columnMaxLength = input[0].length
-
-            // sind alle min 3 von links und rechts entfernt und min 1 von oben und unten
-
-//            if(input[(row - 1).coerceAtLeast(0)][(column)].isDigit()) { // over
-//                input[]
-//            } else {
-//                if (input[(row - 1).coerceAtLeast(0)][(column - 1).coerceAtLeast(0)].isDigit()) { // over left
-//                    TODO()
-//                }
-//                if (input[(row - 1).coerceAtLeast(0)][(column + 1).coerceAtMost(columnMaxLength)].isDigit()) { // over right
-//                    TODO()
-//                }
-//            }
-//            if(input[(row + 1).coerceAtMost(rowMaxLength)][(column + 1).coerceArray(maxLength)].isDigit()) { // under
-//                TODO()
-//            } else {
-//                if (input[(row - 1).coerceArray(maxLength)][(column + 1).coerceArray(maxLength)].isDigit()) { // under left
-//                    TODO()
-//                }
-//                if (input[(row + 1).coerceArray(maxLength)][(column + 1).coerceArray(maxLength)].isDigit()) { // under right
-//                    TODO()
-//                }
-//            }
-//            if(input[(row + 1).coerceArray(maxLength)][column].isDigit()) { // left
-//                TODO()
-//            }
-//            if(input[(row + 1).coerceArray(maxLength)][column].isDigit()) { // right
-//                TODO()
-//            }
-
-//            input[row][column]
-//        }
-
-//        Symbol(index % maxLength, index / maxLength)
-
-
-        println(19/10)
-        println(20/10)
-        println(1 % maxLength)
-        println(8 % maxLength)
-        println(9 % maxLength)
-        println(10 % maxLength)
-        println(11 % maxLength)
-        println(13 % maxLength)
-
-        val test = 0
-        return input.size
+        val numbersNextToSymbols: MutableSet<List<Int>> = mutableSetOf()
+        val parsedInput = input.filter { it.isNotEmpty() || it.isNotBlank() }
+        for (rowIndex in parsedInput.indices) {
+            for ((colIndex, symbol) in parsedInput[rowIndex].withIndex()) {
+                if (symbol == '.' || symbol.isDigit()) continue
+                // all symbols are not on the outer border
+                for (symbolRow in rowIndex - 1 ..  rowIndex + 1) {
+                    for (symbolCol in colIndex - 1 .. colIndex + 1) {
+                        if (!parsedInput[symbolRow][symbolCol].isDigit()) continue
+                        var colOffset: Int = symbolCol
+                        while (true) {
+                            colOffset--
+                            if (!parsedInput[symbolRow][colOffset].isDigit()) {
+                                colOffset++
+                                break
+                            }
+                            if (colOffset == 0) break
+                        }
+                        numbersNextToSymbols.add(listOf(symbolRow, colOffset))
+                    }
+                }
+            }
+        }
+        var sum = 0
+        for (coordinates in numbersNextToSymbols) {
+            val row = coordinates[0]
+            var col = coordinates[1]
+            var sB = StringBuilder()
+            while (parsedInput[row][col].isDigit()) {
+                sB.append(parsedInput[row][col])
+                if (col == parsedInput[row].length - 1) break
+                col++
+            }
+            sum += sB.toString().toInt()
+        }
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-
-        return input.size
+        var sum = 0
+        val parsedInput = input.filter { it.isNotEmpty() || it.isNotBlank() }
+        for (rowIndex in parsedInput.indices) {
+            for ((colIndex, symbol) in parsedInput[rowIndex].withIndex()) {
+                if (symbol != '*') continue
+                val numbersNextToSymbols: MutableSet<List<Int>> = mutableSetOf()
+                // all symbols are not on the outer border
+                for (symbolRow in rowIndex - 1 ..  rowIndex + 1) {
+                    for (symbolCol in colIndex - 1 .. colIndex + 1) {
+                        if (!parsedInput[symbolRow][symbolCol].isDigit()) continue
+                        var colOffset: Int = symbolCol
+                        while (true) {
+                            colOffset--
+                            if (!parsedInput[symbolRow][colOffset].isDigit()) {
+                                colOffset++
+                                break
+                            }
+                            if (colOffset == 0) break
+                        }
+                        numbersNextToSymbols.add(listOf(symbolRow, colOffset))
+                    }
+                }
+                var acc = 1
+                if (numbersNextToSymbols.size == 2) {
+                    for (coordinates in numbersNextToSymbols) {
+                        val row = coordinates[0]
+                        var col = coordinates[1]
+                        var sB = StringBuilder()
+                        while (parsedInput[row][col].isDigit()) {
+                            sB.append(parsedInput[row][col])
+                            if (col == parsedInput[row].length - 1) break
+                            col++
+                        }
+                        acc *= sB.toString().toInt()
+                    }
+                }
+                if (acc != 1) sum += acc
+            }
+        }
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
-    check(part1(testInput) == 142)
-//    check(part2(testInput2) == 281)
+    check(part1(testInput) == 4361)
+    check(part2(testInput) == 467835)
 
     val input = readInput("Day03")
-//    part1(input).println()
-//    part2(input).println()
+    part1(input).println()
+    part2(input).println()
 }
