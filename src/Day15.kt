9cf87f6ccef3451df8efdd4987b889749a3a9ffb
Dr4kn <servicesDrkn@gmail.com>
2023-12-16 16:56:00 +0100

    day 15 p1 done

--- src/Day15.kt
+++ src/Day15.kt
@@ -2,7 +2,14 @@ import java.lang.Exception
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val parsed = input.map { line ->
+            line.split(',').map  hash@{ hashString ->
+                hashString.fold(0) {acc, char ->
+                    ((acc + char.code) * 17) % 256
+                }
+            }
+        }.map { line -> line.reduce{sum, element -> sum + element} }.first()
+        return parsed
     }
     fun part2(input: List<String>): Int {
         return input.size
@@ -10,8 +17,8 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day15_test")
-    check(part1(testInput) == 142)
-    check(part2(testInput) == 281)
+    check(part1(testInput) == 1320)
+//    check(part2(testInput) == 281)
 
     val input = readInput("Day15")
     part1(input).println()
