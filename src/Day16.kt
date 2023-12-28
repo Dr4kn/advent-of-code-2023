68e45ef7dd6ad444bc6249d6b1001a705729c665
Dr4kn <servicesDrkn@gmail.com>
2023-12-28 14:38:58 +0100

    day 16 and 17 ready

---
+++ src/Day16.kt
@@ -0,0 +1,19 @@
+import java.lang.Exception
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day16_test")
+    check(part1(testInput) == 142)
+    check(part2(testInput) == 281)
+
+    val input = readInput("Day16")
+    part1(input).println()
+    part2(input).println()
+}
