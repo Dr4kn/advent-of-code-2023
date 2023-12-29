d8e86e0787f7290c7cc9f117c1ee8be4fca26136
Dr4kn <servicesDrkn@gmail.com>
2023-12-29 14:15:44 +0100

    day 17 part 1

--- src/Day17.kt
+++ src/Day17.kt
@@ -1,8 +1,54 @@
-import java.lang.Exception
+import java.util.PriorityQueue
 
 fun main() {
+    data class Node(val row: Int, val col: Int, val heatloss: Int, var movedRow: Int, var movedCol: Int, val consecutiveMoves: Int)
+
+    fun parseInput(input: List<String>) = input.map { line -> line.map { it.digitToInt() } }
+
+    fun solver(heatMap: List<List<Int>>): Int {
+        val seen = mutableSetOf<List<Int>>()
+        val compareByHeatloss = compareBy<Node> { it.heatloss }
+        val priorityQueue = PriorityQueue(compareByHeatloss)
+        priorityQueue.add(Node(0, 0, 0, 0, 0, 0))
+        val moveDirections = listOf(Pair(0, 1), Pair(0, -1), Pair(1, 0), Pair(-1, 0))
+
+        while (priorityQueue.isNotEmpty()) {
+            val node = priorityQueue.remove()
+            if (node.row == heatMap.size - 1 && node.col == heatMap[0].size -1) return node.heatloss
+
+            if (node.row < 0 || node.row >= heatMap.size || node.col < 0 || node.col >= heatMap[0].size) continue
+
+            val seenList = listOf(node.row, node.col, node.movedRow, node.movedCol, node.consecutiveMoves)
+            if (seen.contains(seenList)) continue // does not contain heatloss to prevent looping
+            seen.add(seenList)
+
+            if (Pair(node.movedRow, node.movedCol) != Pair(0, 0) && node.consecutiveMoves < 3) {
+                val newRow = node.row + node.movedRow
+                val newCol = node.col + node.movedCol
+                if (0 <= newRow && newRow < heatMap.size && 0 <= newCol && newCol < heatMap[0].size) {
+                    priorityQueue.add(Node(newRow, newCol, node.heatloss + heatMap[newRow][newCol],
+                        node.movedRow, node.movedCol, node.consecutiveMoves + 1))
+                }
+            }
+
+
+            for (direction in moveDirections) {
+                if (direction != Pair(node.movedRow, node.movedCol) && direction != Pair(-node.movedRow, -node.movedCol)) {
+                    val newRow = node.row + direction.first
+                    val newCol = node.col + direction.second
+                    if (0 <= newRow && newRow < heatMap.size && 0 <= newCol && newCol < heatMap[0].size) {
+                        priorityQueue.add(Node(newRow, newCol, node.heatloss + heatMap[newRow][newCol],
+                            direction.first, direction.second, 1))
+                    }
+                }
+            }
+        }
+        return 0
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val answer = solver(parseInput(input))
+        return answer
     }
     fun part2(input: List<String>): Int {
         return input.size
@@ -10,8 +56,8 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day17_test")
-    check(part1(testInput) == 142)
-    check(part2(testInput) == 281)
+    check(part1(testInput) == 102)
+//    check(part2(testInput) == 94)
 
     val input = readInput("Day17")
     part1(input).println()
